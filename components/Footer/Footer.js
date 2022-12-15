5d0f47fd62b11db1dfecc8bf0be62c5a9785036c
Altan Guellue <altan.guellue@web.de>
2022-12-15 14:49:44 +0100

    add Footer component and write return statement for home + favorite icon, add to index.js

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,5 +1,40 @@
 import styled from "styled-components";
 import Image from "next/image";
-import HomeImg from "../../assets/images/home.logo";
+import HomeImg from "../../assets/images/home.svg";
+import FavImg from "../../assets/images/favorite.svg";
 
-export default function Footer() {}
+export default function Footer() {
+  return (
+    <StyledHomeWrapper>
+      <StyledHomeIcon
+        src={HomeImg}
+        alt="Home Icon"
+        width={42}
+        height={42}
+        priority
+      />
+      <StyledFavIcon
+        src={FavImg}
+        alt="Favorite Icon"
+        width={51}
+        height={51}
+        priority
+      />
+    </StyledHomeWrapper>
+  );
+}
+
+const StyledHomeWrapper = styled.div`
+  position: fixed;
+  display: flex;
+  bottom: 0;
+  height: 10vh;
+  width: 100%;
+  justify-content: space-evenly;
+  align-items: center;
+
+  background-color: #fae1dd;
+`;
+
+const StyledHomeIcon = styled(Image)``;
+const StyledFavIcon = styled(Image)``;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Header from "../components/Header/Header";
 import FoodList from "../components/Food/FoodList";
+import Footer from "../components/Footer/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <FoodList />
+      <Footer />
     </>
   );
 }
