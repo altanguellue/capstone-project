272552036204ea4f66e399422dbb0314b8f55935
Altan Guellue <altan.guellue@web.de>
2022-12-15 10:35:13 +0100

    add Header component to return statement

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import Header from "../components/Header/Header";
 import FoodList from "../components/Food/FoodList";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <FoodList />
     </>
   );
