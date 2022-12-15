0300744f1fa2179f394aa46d7f8cae505b416c03
Altan Guellue <altan.guellue@web.de>
2022-12-15 10:33:49 +0100

    create Header component, write return function for app logo and style header

---
+++ components/Header/Header.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import Image from "next/image";
+import LogoImg from "../../assets/images/logo.svg";
+
+export default function Header() {
+  return (
+    <StyledLogoWrapper>
+      <Image src={LogoImg} alt="App Logo" width={98} height={62} priority />
+    </StyledLogoWrapper>
+  );
+}
+
+const StyledLogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+`;
+
+const StyledImage = styled(Image)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+`;
