9ec67486d3057d3f717c79ae6a5ee6f443c2b085
Altan Guellue <altan.guellue@web.de>
2022-12-20 10:58:36 +0100

    add search input field (no styles yet), write search function with useState & filter method

--- components/Food/FoodList.js
+++ components/Food/FoodList.js
@@ -1,24 +1,41 @@
 import styled from "styled-components";
+import { useState } from "react";
 import { Fragment } from "react";
 import { dataFoods } from "../../lib/data";
 import FoodCard from "./FoodCard";
 
 export default function FoodList() {
+  const [search, setSearch] = useState("");
   return (
     <>
+      <StyledSearchInput>
+        <input
+          type="text"
+          placeholder="Suchen"
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+        />
+      </StyledSearchInput>
+
       <StyledFoodListHeader>Obst & Gemüse</StyledFoodListHeader>
+
       <StyledFoodList>
-        {dataFoods.map((dataFood) => {
-          return (
-            <Fragment key={dataFood.id}>
-              <FoodCard
-                id={dataFood.id}
-                name={dataFood.name}
-                img={dataFood.img}
-              />
-            </Fragment>
-          );
-        })}
+        {dataFoods
+          .filter((dataFood) => {
+            return search.toLowerCase() === ""
+              ? dataFood
+              : dataFood.name.toLowerCase().includes(search);
+          })
+          .map((dataFood) => {
+            return (
+              <Fragment key={dataFood.id}>
+                <FoodCard
+                  id={dataFood.id}
+                  name={dataFood.name}
+                  img={dataFood.img}
+                />
+              </Fragment>
+            );
+          })}
       </StyledFoodList>
     </>
   );
@@ -35,4 +52,11 @@ const StyledFoodListHeader = styled.h2`
 const StyledFoodList = styled.ul`
   display: flex;
   flex-direction: column;
+`;
+
+const StyledSearchInput = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 20px;
+  justify-content: center;
 `;
