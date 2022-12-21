81dffe27efd03dec39fcd3d6981874bac860c69c
Altan Guellue <altan.guellue@web.de>
2022-12-21 11:23:56 +0100

    add search-icon.svg to assets, style searchbar

--- components/Food/FoodList.js
+++ components/Food/FoodList.js
@@ -3,14 +3,23 @@ import { useState } from "react";
 import { Fragment } from "react";
 import { dataFoods } from "../../lib/data";
 import FoodCard from "./FoodCard";
+import Image from "next/image";
+import SearchIcon from "../../assets/images/search-icon.svg";
 
 export default function FoodList() {
   const [search, setSearch] = useState("");
   return (
     <>
+      <StyledHomeHeader>Dezember</StyledHomeHeader>
       <StyledSearchInput>
+        <StyledSearchIcon
+          src={SearchIcon}
+          alt="Search Icon"
+          width={20}
+          height={20}
+        />
         <input
-          type="text"
+          type="search"
           placeholder="Suchen"
           onChange={(e) => setSearch(e.target.value.toLowerCase())}
         />
@@ -41,22 +50,58 @@ export default function FoodList() {
   );
 }
 
+const StyledHomeHeader = styled.h1`
+  margin-left: 0.5rem;
+
+  color: #71554f;
+  font-weight: 100;
+  font-size: 26px;
+  line-height: 39px;
+`;
 const StyledFoodListHeader = styled.h2`
-  text-align: left;
+  margin-left: 0.5rem;
   color: #71554f;
   font-weight: 300;
   font-size: 18px;
   line-height: 22px;
-  padding-left: 12px;
 `;
 const StyledFoodList = styled.ul`
   display: flex;
   flex-direction: column;
+  padding-bottom: 90px;
 `;
 
 const StyledSearchInput = styled.div`
   display: flex;
   width: 100%;
-  padding: 20px;
+  padding: 0 0 10px 0;
   justify-content: center;
+  outline: none;
+
+  input::placeholder {
+    color: #ffe6e1;
+  }
+
+  input {
+    margin: 0.5rem;
+    padding: 5px;
+    border-radius: 6px;
+    border: none;
+    width: 100%;
+    background-color: #e8a598;
+    color: #ffe6e1;
+    padding-left: 25px;
+  }
+  input:focus {
+    outline: none;
+  }
+  input::-webkit-search-cancel-button {
+    appearance: none;
+  }
+`;
+const StyledSearchIcon = styled(Image)`
+  position: absolute;
+  left: 10px;
+  align-self: center;
+  justify-self: self-start;
 `;
