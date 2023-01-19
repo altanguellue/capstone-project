51d4148e21c298c31a386ff14c39498bc3eb745c
Altan Güllü <115747088+altanguellue@users.noreply.github.com>
2023-01-19 16:08:48 +0100

    FilterFunction (#14)
    * create filter function for food category and availability
    
    * Add Styling to Button and add active class function
    
    * remove info-icon and change info for apple and mushrooms
    
    * refactoring

--- components/Food/FoodList.js
+++ components/Food/FoodList.js
@@ -7,9 +7,34 @@ import SearchIcon from "../../assets/images/search-icon.svg";
 
 export default function FoodList({ onToggleFavorite, foods }) {
   const [search, setSearch] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [selectedMonth, setSelectedMonth] = useState(null);
+
+  let filteredItems = [...foods];
+  let monthText = "Gesamtes Jahr";
+  let foodText = "Obst & Gemüse";
+
+  filteredItems.sort((foodA, foodB) => {
+    if (foodA.name > foodB.name) return 1;
+    if (foodA.name < foodB.name) return -1;
+    return 0;
+  });
+
+  if (selectedCategory !== "All") {
+    filteredItems = foods.filter((food) => food.category === selectedCategory);
+    foodText = `${selectedCategory}`;
+  }
+
+  if (selectedMonth) {
+    filteredItems = filteredItems.filter((food) =>
+      food.availability.includes(selectedMonth)
+    );
+    monthText = `${selectedMonth}`;
+  }
+
   return (
     <>
-      <StyledHomeHeader>Dezember</StyledHomeHeader>
+      <StyledHomeHeader>{monthText}</StyledHomeHeader>
       <StyledSearchInput>
         <StyledSearchIcon
           src={SearchIcon}
@@ -23,11 +48,107 @@ export default function FoodList({ onToggleFavorite, foods }) {
           onChange={(e) => setSearch(e.target.value.toLowerCase())}
         />
       </StyledSearchInput>
+      <StyledButtonContainer>
+        <StyledCategoryButtonContainer>
+          <StyledCategoryButton
+            className={selectedCategory === "All" ? "active" : ""}
+            onClick={() => setSelectedCategory("All")}
+          >
+            Alle
+          </StyledCategoryButton>
+          <StyledCategoryButton
+            className={selectedCategory === "Gemüse" ? "active" : ""}
+            onClick={() => setSelectedCategory("Gemüse")}
+          >
+            Gemüse
+          </StyledCategoryButton>
+          <StyledCategoryButton
+            className={selectedCategory === "Obst" ? "active" : ""}
+            onClick={() => setSelectedCategory("Obst")}
+          >
+            Obst
+          </StyledCategoryButton>
+        </StyledCategoryButtonContainer>
 
-      <StyledFoodListHeader>Obst & Gemüse</StyledFoodListHeader>
+        <StyledMonthButtonContainer>
+          <StyledMonthButton
+            className={selectedMonth === "Januar" ? "active" : ""}
+            onClick={() => setSelectedMonth("Januar")}
+          >
+            Jan
+          </StyledMonthButton>
+          <StyledMonthButton
+            className={selectedMonth === "Februar" ? "active" : ""}
+            onClick={() => setSelectedMonth("Februar")}
+          >
+            Feb
+          </StyledMonthButton>
+          <StyledMonthButton
+            className={selectedMonth === "März" ? "active" : ""}
+            onClick={() => setSelectedMonth("März")}
+          >
+            Mär
+          </StyledMonthButton>
+          <StyledMonthButton
+            className={selectedMonth === "April" ? "active" : ""}
+            onClick={() => setSelectedMonth("April")}
+          >
+            Apr
+          </StyledMonthButton>
+          <StyledMonthButton
+            className={selectedMonth === "Mai" ? "active" : ""}
+            onClick={() => setSelectedMonth("Mai")}
+          >
+            Mai
+          </StyledMonthButton>
+          <StyledMonthButton
+            className={selectedMonth === "Juni" ? "active" : ""}
+            onClick={() => setSelectedMonth("Juni")}
+          >
+            Jun
+          </StyledMonthButton>
+          <StyledMonthButton
+            className={selectedMonth === "Juli" ? "active" : ""}
+            onClick={() => setSelectedMonth("Juli")}
+          >
+            Jul
+          </StyledMonthButton>
+          <StyledMonthButton
+            className={selectedMonth === "August" ? "active" : ""}
+            onClick={() => setSelectedMonth("August")}
+          >
+            Aug
+          </StyledMonthButton>
+          <StyledMonthButton
+            className={selectedMonth === "September" ? "active" : ""}
+            onClick={() => setSelectedMonth("September")}
+          >
+            Sep
+          </StyledMonthButton>
+          <StyledMonthButton
+            className={selectedMonth === "Oktober" ? "active" : ""}
+            onClick={() => setSelectedMonth("Oktober")}
+          >
+            Okt
+          </StyledMonthButton>
+          <StyledMonthButton
+            className={selectedMonth === "November" ? "active" : ""}
+            onClick={() => setSelectedMonth("November")}
+          >
+            Nov
+          </StyledMonthButton>
+          <StyledMonthButton
+            className={selectedMonth === "Dezember" ? "active" : ""}
+            onClick={() => setSelectedMonth("Dezember")}
+          >
+            Dez
+          </StyledMonthButton>
+        </StyledMonthButtonContainer>
+      </StyledButtonContainer>
+      <StyledFoodListHeader>{foodText}</StyledFoodListHeader>
 
       <StyledFoodList>
-        {foods
+        {filteredItems
           .filter((food) => {
             return search.toLowerCase() === ""
               ? food
@@ -62,8 +183,9 @@ const StyledFoodListHeader = styled.h2`
   margin-left: 0.5rem;
   color: #71554f;
   font-weight: 300;
-  font-size: 18px;
+  font-size: 20px;
   line-height: 22px;
+  padding-top: 15px;
 `;
 const StyledFoodList = styled.ul`
   display: flex;
@@ -104,4 +226,53 @@ const StyledSearchIcon = styled(Image)`
   left: 10px;
   align-self: center;
   justify-self: self-start;
+`;
+
+const StyledButtonContainer = styled.div`
+  margin: 0.5rem;
+  background-color: #e8a598;
+  border-radius: 6px;
+`;
+
+const StyledCategoryButtonContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  border-radius: 6px;
+`;
+
+const StyledMonthButtonContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+`;
+
+const StyledCategoryButton = styled.button`
+  background-color: transparent;
+  border: none;
+  color: #71554f;
+  padding: 0.4rem;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 18px;
+  margin: 0.2rem;
+  background-color: #fec5bb;
+  border-radius: 6px;
+  &.active {
+    background-color: #f9dcd7;
+  }
+`;
+
+const StyledMonthButton = styled.button`
+  background-color: transparent;
+  border: none;
+  color: #71554f;
+  padding: 0.4rem;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 18px;
+  margin: 0.2rem;
+  &.active {
+    background-color: #f9dcd7;
+    border-radius: 6px;
+  }
 `;
